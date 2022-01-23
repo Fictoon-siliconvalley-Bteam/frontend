cb7a163e0a328365bc6c0d54b564c4d2ed2ad2cb
KimJungYeon <jungyeon9130@kpu.ac.kr>
2022-01-23 17:59:20 +0900

    fix : loading,s3 connect
    로딩화면, s3연결, nft마켓 링크연결

--- frontend/src/components/Result/Resultpage.js
+++ frontend/src/components/Result/Resultpage.js
@@ -1,21 +1,32 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react'
-import {Img,Column2,ImgWrap,ResultContainer} from '../Result/ResultElements'
+import { Link } from 'react-router-dom'
+import {Column2,ImgWrap,ResultContainer,Heading,Subtitle} from '../Result/ResultElements'
 import Spinner from '../Spinner/Spinnerindex'
 
-export default function Result(){
+export default function Result({lightBg,id,imgStart,topLine,lightText,headLine,darkText,description,alt}){
     const [image,setImage] = useState([]);
     const [loading,setLoading] = useState(true);
     setTimeout(() => {
         setLoading(false);
-    },3000);
+    },20000); //1000초당 1초
    
     return (
         <ResultContainer>
          <Column2>
                 <ImgWrap>
                 {loading ? <Spinner /> :
-                     <img src="https://fictoonimage.s3.ap-northeast-2.amazonaws.com/out.png.jpg"/>}
+                   <>
+                   <Heading lightText={lightText}>{headLine}</Heading>
+                   <Subtitle darkText={darkText}>{description}</Subtitle>
+                   <Link to='//nft.korbit.co.kr/'>Go to NFT Market </Link>
+                   <br/><br/>
+                   <img src="https://fictoonimage.s3.ap-northeast-2.amazonaws.com/out"
+                            width="600px"
+                            height="500px" />
+                        </> }
+               
+                     
                 </ImgWrap>
          </Column2>
          </ResultContainer>
